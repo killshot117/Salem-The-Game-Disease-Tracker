a4f5b927e239f562033c8726446a62e75c923e19
Tristyn Naeole <tristynnaeole@yahoo.com>
2021-06-20 00:29:12 -0700

    Add project files.

---
+++ SalemTheGameDiseaseTracker/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace SalemTheGameDiseaseTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cough = new System.Windows.Forms.Label();
+            this.Dizziness = new System.Windows.Forms.Label();
+            this.Vomiting = new System.Windows.Forms.Label();
+            this.Headache = new System.Windows.Forms.Label();
+            this.CoughCount = new System.Windows.Forms.Label();
+            this.DizzinessCount = new System.Windows.Forms.Label();
+            this.VomitingCount = new System.Windows.Forms.Label();
+            this.HeadacheCount = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Salem Disease Tracker";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Cough
+            // 
+            this.Cough.AutoSize = true;
+            this.Cough.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cough.Location = new System.Drawing.Point(12, 59);
+            this.Cough.Name = "Cough";
+            this.Cough.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Cough.Size = new System.Drawing.Size(76, 17);
+            this.Cough.TabIndex = 1;
+            this.Cough.Text = "Coughing";
+            // 
+            // Dizziness
+            // 
+            this.Dizziness.AutoSize = true;
+            this.Dizziness.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dizziness.Location = new System.Drawing.Point(90, 59);
+            this.Dizziness.Name = "Dizziness";
+            this.Dizziness.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Dizziness.Size = new System.Drawing.Size(77, 17);
+            this.Dizziness.TabIndex = 2;
+            this.Dizziness.Text = "Dizziness\t";
+            // 
+            // Vomiting
+            // 
+            this.Vomiting.AutoSize = true;
+            this.Vomiting.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vomiting.Location = new System.Drawing.Point(164, 59);
+            this.Vomiting.Name = "Vomiting";
+            this.Vomiting.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Vomiting.Size = new System.Drawing.Size(70, 17);
+            this.Vomiting.TabIndex = 3;
+            this.Vomiting.Text = "Vomiting\t";
+            // 
+            // Headache
+            // 
+            this.Headache.AutoSize = true;
+            this.Headache.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Headache.Location = new System.Drawing.Point(232, 59);
+            this.Headache.Name = "Headache";
+            this.Headache.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Headache.Size = new System.Drawing.Size(81, 17);
+            this.Headache.TabIndex = 4;
+            this.Headache.Text = "Headache";
+            // 
+            // CoughCount
+            // 
+            this.CoughCount.AutoSize = true;
+            this.CoughCount.Location = new System.Drawing.Point(16, 92);
+            this.CoughCount.Name = "CoughCount";
+            this.CoughCount.Size = new System.Drawing.Size(35, 13);
+            this.CoughCount.TabIndex = 5;
+            this.CoughCount.Text = "label2";
+            // 
+            // DizzinessCount
+            // 
+            this.DizzinessCount.AutoSize = true;
+            this.DizzinessCount.Location = new System.Drawing.Point(87, 92);
+            this.DizzinessCount.Name = "DizzinessCount";
+            this.DizzinessCount.Size = new System.Drawing.Size(35, 13);
+            this.DizzinessCount.TabIndex = 6;
+            this.DizzinessCount.Text = "label3";
+            // 
+            // VomitingCount
+            // 
+            this.VomitingCount.AutoSize = true;
+            this.VomitingCount.Location = new System.Drawing.Point(161, 92);
+            this.VomitingCount.Name = "VomitingCount";
+            this.VomitingCount.Size = new System.Drawing.Size(35, 13);
+            this.VomitingCount.TabIndex = 7;
+            this.VomitingCount.Text = "label4";
+            // 
+            // HeadacheCount
+            // 
+            this.HeadacheCount.AutoSize = true;
+            this.HeadacheCount.Location = new System.Drawing.Point(229, 92);
+            this.HeadacheCount.Name = "HeadacheCount";
+            this.HeadacheCount.Size = new System.Drawing.Size(35, 13);
+            this.HeadacheCount.TabIndex = 8;
+            this.HeadacheCount.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "+1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "+1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "+1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(232, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "+1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(333, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Reset";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 153);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "-1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(81, 153);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "-1";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(164, 153);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 23);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "-1";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(232, 153);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 23);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "-1";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 192);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.HeadacheCount);
+            this.Controls.Add(this.VomitingCount);
+            this.Controls.Add(this.DizzinessCount);
+            this.Controls.Add(this.CoughCount);
+            this.Controls.Add(this.Headache);
+            this.Controls.Add(this.Vomiting);
+            this.Controls.Add(this.Dizziness);
+            this.Controls.Add(this.Cough);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Salem Disease Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Cough;
+        private System.Windows.Forms.Label Dizziness;
+        private System.Windows.Forms.Label Vomiting;
+        private System.Windows.Forms.Label Headache;
+        private System.Windows.Forms.Label CoughCount;
+        private System.Windows.Forms.Label DizzinessCount;
+        private System.Windows.Forms.Label VomitingCount;
+        private System.Windows.Forms.Label HeadacheCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ SalemTheGameDiseaseTracker/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalemTheGameDiseaseTracker
+{
+    public partial class Form1 : Form
+    {
+        int cough = 0, vomit = 0, dizzy = 0, headache = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cough = cough + 1;
+            CoughCount.Text = Convert.ToString(cough);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dizzy = dizzy + 1;
+            DizzinessCount.Text = Convert.ToString(dizzy);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vomit = vomit + 1;
+            VomitingCount.Text = Convert.ToString(vomit);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            headache = headache + 1;
+            HeadacheCount.Text = Convert.ToString(headache);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cough = cough - 1;
+            CoughCount.Text = Convert.ToString(cough);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dizzy = dizzy - 1;
+            DizzinessCount.Text = Convert.ToString(dizzy);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            vomit = vomit - 1;
+            VomitingCount.Text = Convert.ToString(vomit);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            headache = headache - 1;
+            HeadacheCount.Text = Convert.ToString(headache);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cough = 0;
+            vomit = 0;
+            dizzy = 0;
+            headache = 0;
+            HeadacheCount.Text = Convert.ToString(headache);
+            VomitingCount.Text = Convert.ToString(vomit);
+            DizzinessCount.Text = Convert.ToString(dizzy);
+            CoughCount.Text = Convert.ToString(cough);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            CoughCount.Text = Convert.ToString(0);
+            VomitingCount.Text = Convert.ToString(0);
+            DizzinessCount.Text = Convert.ToString(0);
+            HeadacheCount.Text = Convert.ToString(0);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
